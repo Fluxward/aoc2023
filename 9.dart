3a6b044ecfb8682f3754e8f3d2018cda673e1433
Fluxward <howard@fluxkeyboard.com>
2023-12-09 17:09:04 +1100

    make it dartier

--- 9.dart
+++ 9.dart
@@ -1,47 +1,22 @@
 import 'common.dart';
 
 d9(bool s) {
-  List<String> lines = getLines();
-  List<List<int>> hist = lines.map((e) => stois(e)).toList();
-
-  s ? b9(hist) : a9(hist);
-}
-
-a9(List<List<int>> hist) {
-  print(hist.fold<int>(
-      0, (previousValue, element) => previousValue + predict(diffs(element))));
-}
-
-b9(List<List<int>> hist) {
-  print(hist.fold<int>(0,
-      (previousValue, element) => previousValue + backPredict(diffs(element))));
-}
-
-int predict(List<List<int>> diffs) {
-  return diffs.reversed.fold(0, (p, e) => p + e.last);
-}
-
-int backPredict(List<List<int>> diffs) {
-  return diffs.reversed.fold(0, (p, e) => e.first - p);
+  (List<List<int>> hist) {
+    print(hist.fold<int>(
+        0,
+        (p, e) =>
+            p +
+            diffs(e).reversed.fold(0, (p, e) => s ? e.first - p : p + e.last)));
+  }(getLines().map((e) => stois(e)).toList());
 }
 
 List<List<int>> diffs(List<int> hist) {
   List<List<int>> ret = [];
-  ret.add(hist);
-  bool done = false;
 
-  List<int> cur = hist;
-  while (!done) {
-    cur = getDiffs(cur);
-    ret.add(cur);
-    if (cur.every((element) => element == 0)) {
-      done = true;
-    }
-  }
+  do {
+    ret.add(hist);
+    hist = List.generate(hist.length - 1, (i) => hist[i + 1] - hist[i]);
+  } while (!hist.every((element) => element == 0));
 
   return ret;
-}
-
-List<int> getDiffs(List<int> hist) {
-  return List.generate(hist.length - 1, (i) => hist[i + 1] - hist[i]);
 }
