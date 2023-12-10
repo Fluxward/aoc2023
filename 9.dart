35af7bd7347ff5b1cf8f711d0c62355295fd9547
Fluxward <howard@fluxkeyboard.com>
2023-12-10 13:27:59 +1100

    no nulls needed

--- 9.dart
+++ 9.dart
@@ -2,14 +2,14 @@ import 'common.dart';
 
 d9(bool s) {
   print(getLines().fold<int>(0, (p, e) {
-    int? pre(List<int> h, bool s) {
+    int pre(List<int> h, bool s) {
       return h.every((e) => e == 0)
-          ? null
-          : (pre(List.generate(h.length - 1, (i) => h[i + 1] - h[i]), s) ?? 0) *
+          ? 0
+          : (pre(List.generate(h.length - 1, (i) => h[i + 1] - h[i]), s)) *
                   (s ? -1 : 1) +
               (s ? h.first : h.last);
     }
 
-    return p + pre(stois(e), s)!;
+    return p + pre(stois(e), s);
   }));
 }
