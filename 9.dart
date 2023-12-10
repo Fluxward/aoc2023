35ee0b1be327833debc7bc3c2766eaa4f08e2dad
Fluxward <howard@fluxkeyboard.com>
2023-12-10 13:24:29 +1100

    even dartier

--- 9.dart
+++ 9.dart
@@ -1,19 +1,15 @@
 import 'common.dart';
 
 d9(bool s) {
-  print(getLines().fold<int>(
-      0,
-      (p, e) =>
-          p + diffs(stois(e)).fold(0, (p, e) => s ? e.first - p : p + e.last)));
-}
+  print(getLines().fold<int>(0, (p, e) {
+    int? pre(List<int> h, bool s) {
+      return h.every((e) => e == 0)
+          ? null
+          : (pre(List.generate(h.length - 1, (i) => h[i + 1] - h[i]), s) ?? 0) *
+                  (s ? -1 : 1) +
+              (s ? h.first : h.last);
+    }
 
-List<List<int>> diffs(List<int> hist) {
-  List<List<int>> ret = [];
-
-  do {
-    ret.add(hist);
-    hist = List.generate(hist.length - 1, (i) => hist[i + 1] - hist[i]);
-  } while (!hist.every((element) => element == 0));
-
-  return ret.reversed.toList();
+    return p + pre(stois(e), s)!;
+  }));
 }
