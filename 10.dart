b4d4a9200da5d6b4c95769dd7d799d4353b42b14
Howard Seatang <howard@fluxkeyboard.com>
2023-12-11 00:00:28 +1100

    commit broken code

--- 10.dart
+++ 10.dart
@@ -15,6 +15,192 @@ Point<int>? findS(List<String> m) {
   return null;
 }
 
+enum dir {
+  u(Point<int>(-1, 0)),
+  d(Point<int>(1, 0)),
+  l(Point<int>(0, -1)),
+  r(Point<int>(0, 1));
+
+  final Point<int> p;
+  const dir(this.p);
+}
+
+enum pT {
+  // x is the row index, y is the column index.
+  ud(dir.u, dir.d),
+  lr(dir.l, dir.r),
+  ur(dir.u, dir.r),
+  ul(dir.u, dir.l),
+  dr(dir.d, dir.r),
+  dl(dir.d, dir.l);
+
+  final dir a;
+  final dir b;
+
+  const pT(this.a, this.b);
+
+  bool connected(Point<int> d) => a == d * -1 || b == d * -1;
+
+  static pT? fromString(String s) {
+    switch (s) {
+      case '|':
+        return ud;
+      case '-':
+        return lr;
+      case 'L':
+        return ur;
+      case 'F':
+        return dr;
+      case 'J':
+        return ul;
+      case '7':
+        return dl;
+    }
+    return null;
+  }
+}
+
+bool inBounds(int x, int y, List<String> m) =>
+    x >= 0 && y >= 0 && x < m.length && y < m[0].length;
+
+bool pInBounds(Point<int> p, List<String> m) => inBounds(p.x, p.y, m);
+
+pT getStartPipeType(Point<int> s, List<String> m) {
+  // determine pipe type of S
+  pT? u = pT.fromString(m[s.x - 1][s.y]);
+  pT? d = pT.fromString(m[s.x + 1][s.y]);
+  pT? l = pT.fromString(m[s.x][s.y - 1]);
+
+  bool bu = false;
+  bool bd = false;
+  bool bl = false;
+
+  if (u != null) {
+    bu = u.connected(Point<int>(-1, 0));
+  }
+  if (d != null) {
+    bd = d.connected(Point<int>(1, 0));
+  }
+  if (l != null) {
+    bl = l.connected(Point<int>(0, -1));
+  }
+
+  return bu
+      ? (bd ? pT.ud : (bl ? pT.ul : (pT.ur)))
+      : (bd ? (bl ? pT.dl : pT.dr) : pT.lr);
+}
+
+List<Point<int>>? getPath(List<String> m) {
+  Point<int>? s = findS(m);
+  if (s == null) {
+    print("Couldn't find s!");
+    return null;
+  }
+
+  int length = 0;
+
+  // dfs the path
+  Point<int> cur = s;
+  Point<int>? next;
+  dir? prevD = null;
+  List<Point<int>> path = [s];
+  while ((prevD == null || cur != s)) {
+    for (int i = 0; i < 4; i++) {
+      pT? curP = pT.fromString(m[cur.x][cur.y]);
+      dir d = dir.values[i];
+      // don't check backwards
+      if (prevD == d ||
+          // don't check an adjacent pipe if it isn't connected to this
+          (cur != s && curP != null && !curP.connected(dir.values[i].p * -1))) {
+        continue;
+      }
+
+      next = cur + ds[i];
+
+      if (next.x < 0 ||
+          next.y < 0 ||
+          next.x >= m.length ||
+          next.y >= m.length) {
+        continue;
+      }
+
+      if (next == s) {
+        length++;
+        print("$length, ${length ~/ 2}");
+        return path;
+      }
+
+      pT? np = pT.fromString(m[next.x][next.y]);
+      if (np == null) {
+        continue;
+      }
+
+      if (np.connected(ds[i])) {
+        cur = next;
+        prevD = ds[i] * -1;
+        length++;
+        path.add(cur);
+        break;
+      }
+    }
+  }
+
+  return null;
+}
+
+void a10(List<String> m) {
+  print(getPath(m)!.length ~/ 2);
+}
+
+void b10(List<String> m) {
+  List<Point<int>>? p = getPath(m);
+  if (p == null) {
+    return;
+  }
+
+  Map<Point<int>, pT> path = Map.fromIterable(p,
+      key: (p) => p, value: (p) => p == s ? spt : pT.fromString(m[p.x][p.y])!);
+
+  // track vertical crossings, horizontal crossings.
+  Map<int, Set<int>> inside = {};
+
+  int count = 0;
+  for (int i = 0; i < m.length; i++) {
+    bool outside = true;
+    pT? prevCorner = null;
+    for (int j = 0; j < m[i].length; j++) {
+      pT? c = path[Point<int>(i, j)];
+      if (c != null) {
+        switch (c) {
+          case pT.ud:
+            outside = !outside;
+            break;
+          case pT.ur:
+          case pT.dr:
+            prevCorner = c;
+          case pT.ul:
+            if (prevCorner == pT.dr) {
+              outside = !outside;
+            }
+            prevCorner = null;
+          case pT.dl:
+            if (prevCorner == pT.ur) {
+              outside = !outside;
+            }
+          case pT.lr:
+          // fall through
+        }
+        continue;
+      }
+      if (!outside) {
+        count += 1;
+        inside.putIfAbsent(i, () => {})..add(j);
+      }
+    }
+  }
+  print(count);
+}
+
 void printGrid(List<String> m, int x, int y) {
   print("-" * m.length);
   for (int i = 0; i < m.length; i++) {
@@ -98,188 +284,4 @@ void modSB(
       b1.write(' | ');
       b2.write('-* ');
   }
-}
-
-enum pT {
-  // x is the row index, y is the column index.
-  ud(0, -1, 0, 1),
-  lr(-1, 0, 1, 0),
-  ur(0, -1, 1, 0),
-  ul(0, -1, -1, 0),
-  dr(0, 1, 1, 0),
-  dl(0, 1, -1, 0);
-
-  final int yda;
-  final int xda;
-  final int ydb;
-  final int xdb;
-
-  const pT(this.yda, this.xda, this.ydb, this.xdb);
-
-  bool connected(Point<int> d) {
-    if (yda == -d.y && xda == -d.x) {
-      return true;
-    }
-
-    if (ydb == -d.y && xdb == -d.x) {
-      return true;
-    }
-
-    return false;
-  }
-
-  static pT? fromString(String s) {
-    switch (s) {
-      case '|':
-        return ud;
-      case '-':
-        return lr;
-      case 'L':
-        return ur;
-      case 'F':
-        return dr;
-      case 'J':
-        return ul;
-      case '7':
-        return dl;
-    }
-    return null;
-  }
-}
-
-List<Point<int>> ds = [
-  Point(1, 0),
-  Point(0, 1),
-  Point(-1, 0),
-  Point(0, -1),
-];
-
-List<Point<int>>? a10(List<String> m) {
-  Point<int>? s = findS(m);
-  if (s == null) {
-    print("Couldn't find s!");
-    return null;
-  }
-
-  int length = 0;
-
-  // find next pipe
-  Point<int> cur = s;
-  Point<int>? next;
-  Point<int>? prevD = null;
-  List<Point<int>> path = [s];
-  while ((prevD == null || cur != s)) {
-    for (int i = 0; i < 4; i++) {
-      pT? curP = pT.fromString(m[cur.x][cur.y]);
-      if ((prevD != null && ds[i] == prevD) ||
-          (cur != s && curP != null && !curP.connected(ds[i] * -1))) {
-        continue;
-      }
-
-      next = cur + ds[i];
-
-      if (next.x < 0 ||
-          next.y < 0 ||
-          next.x >= m.length ||
-          next.y >= m.length) {
-        continue;
-      }
-
-      if (next == s) {
-        length++;
-        print("$length, ${length ~/ 2}");
-        return path;
-      }
-
-      pT? np = pT.fromString(m[next.x][next.y]);
-      if (np == null) {
-        continue;
-      }
-
-      if (np.connected(ds[i])) {
-        cur = next;
-        prevD = ds[i] * -1;
-        length++;
-        path.add(cur);
-        break;
-      }
-    }
-  }
-  print(length);
-}
-
-b10(List<String> m) {
-  List<Point<int>>? p = a10(m);
-  if (p == null) {
-    return;
-  }
-
-  // determine pipe type of S
-  Point<int> s = p[0];
-  pT? u = pT.fromString(m[s.x - 1][s.y]);
-  pT? d = pT.fromString(m[s.x + 1][s.y]);
-  pT? l = pT.fromString(m[s.x][s.y - 1]);
-  pT? r = pT.fromString(m[s.x][s.y + 1]);
-
-  bool bu = false;
-  bool bd = false;
-  bool bl = false;
-  bool br = false;
-
-  if (u != null) {
-    bu = u.connected(Point<int>(-1, 0));
-  }
-  if (d != null) {
-    bd = d.connected(Point<int>(1, 0));
-  }
-  if (l != null) {
-    bl = l.connected(Point<int>(0, -1));
-  }
-
-  pT spt = bu
-      ? (bd ? pT.ud : (bl ? pT.ul : (pT.ur)))
-      : (bd ? (bl ? pT.dl : pT.dr) : pT.lr);
-
-  Map<Point<int>, pT> path = Map.fromIterable(p,
-      key: (p) => p, value: (p) => p == s ? spt : pT.fromString(m[p.x][p.y])!);
-
-  // track vertical crossings, horizontal crossings.
-  Map<int, Set<int>> inside = {};
-
-  int count = 0;
-  for (int i = 0; i < m.length; i++) {
-    bool outside = true;
-    pT? prevCorner = null;
-    for (int j = 0; j < m[i].length; j++) {
-      pT? c = path[Point<int>(i, j)];
-      if (c != null) {
-        switch (c) {
-          case pT.ud:
-            outside = !outside;
-            break;
-          case pT.ur:
-          case pT.dr:
-            prevCorner = c;
-          case pT.ul:
-            if (prevCorner == pT.dr) {
-              outside = !outside;
-            }
-            prevCorner = null;
-          case pT.dl:
-            if (prevCorner == pT.ur) {
-              outside = !outside;
-            }
-          case pT.lr:
-          // fall through
-        }
-        continue;
-      }
-      if (!outside) {
-        count += 1;
-        inside.putIfAbsent(i, () => {})..add(j);
-      }
-    }
-  }
-  printArea(m, inside, path.keys.toSet());
-  print(count);
 }
