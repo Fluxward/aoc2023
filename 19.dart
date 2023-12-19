638ccfb5e51433d086f7e568e56cb165609141e7
Howard Seatang <howard@fluxkeyboard.com>
2023-12-19 22:26:24 +1100

    true final cleanup

--- 19.dart
+++ 19.dart
@@ -3,24 +3,21 @@ import 'common.dart';
 typedef Part = List<int>;
 typedef Range = List<(int, int)>;
 
-d19(bool s) {
+d19(bool _) {
   List<String> ls = getLines();
   Map<String, List<Rule>> wfs = Map.fromEntries(
       [for (int i = 0; ls[i].isNotEmpty; i++) parseWorkflow(ls[i])]);
 
-  if (!s) {
-    print([
-      for (int i = wfs.length + 1; i < ls.length; i++)
-        ls[i]
-            .substring(1, ls[i].length - 1)
-            .split(',')
-            .map((e) => int.parse(e.split('=')[1]))
-            .toList()
-    ]
-        .where((p) => accepted(p, "in", wfs))
-        .fold<int>(0, (p, e) => p + e.fold(0, (p, e) => p + e)));
-    return;
-  }
+  print([
+    for (int i = wfs.length + 1; i < ls.length; i++)
+      ls[i]
+          .substring(1, ls[i].length - 1)
+          .split(',')
+          .map((e) => int.parse(e.split('=')[1]))
+          .toList()
+  ]
+      .where((p) => accepted(p, "in", wfs))
+      .fold<int>(0, (p, e) => p + e.fold(0, (p, e) => p + e)));
 
   print(rangeTester([(1, 4001), (1, 4001), (1, 4001), (1, 4001)], "in", wfs)
       .fold<int>(0, (p, e) => p + e.fold<int>(1, (p, e) => p * (e.$2 - e.$1))));
@@ -55,9 +52,7 @@ bool accepted(Part p, String cwf, Map<String, List<Rule>> wfs) {
 
   for (int i = 0; i < ts.length; i++) {
     Jump r = ts[i].test(p);
-    if (!r.hasResult) {
-      continue;
-    }
+    if (!r.hasResult) continue;
     return r.des != null ? accepted(p, r.des!, wfs) : r.aed == true;
   }
   return false;
@@ -133,9 +128,8 @@ class CompRule implements Rule {
   List<tr> rtest(Range p) {
     int split = v + (lt ? 0 : 1);
 
-    if (p[m].$1 > split || p[m].$2 <= split) {
+    if (p[m].$1 > split || p[m].$2 <= split)
       return [tr(p, (lt ? p[m].$2 < v : p[m].$2 > v) ? res : Jump())];
-    }
 
     Range l1 = Range.of(p);
     Range l2 = Range.of(p);
