a14187a13c9268cf383f944479dadbc3830323cc
Fluxward <howard@fluxkeyboard.com>
2024-12-06 19:57:14 +1100

    compress 5

--- src/2024/5.dart
+++ src/2024/5.dart
@@ -1,51 +1,26 @@
 import '../common.dart';
 import 'package:collection/collection.dart';
 
-Map<int, List<Set<int>>> ordering = {};
-int nline = 0;
+void d5(bool isPart2) {
+  Map<int, Set<int>> ordering = {};
+  int comp(int a, int b) => ordering[a]?.contains(b) ?? false ? -1 : 1;
 
-void d5(bool subset) {
   List<String> lines = getLines();
-  for (String line = lines.first; line.contains('|'); line = lines[++nline]) {
-    List<int> rule = line.split('|').map((e) => int.parse(e)).toList();
+  int rulesEnd = lines.indexWhere((line) => line.isEmpty);
 
-    ordering.putIfAbsent(rule[0], () => [{}, {}]);
-    ordering.putIfAbsent(rule[1], () => [{}, {}]);
+  lines
+      .sublist(0, rulesEnd)
+      .map((l) => l.split('|').map((e) => int.parse(e)).toList())
+      .forEach((r) => ordering.putIfAbsent(r[0], Set.new).add(r[1]));
 
-    ordering[rule[0]]![0].add(rule[1]);
-    ordering[rule[1]]![1].add(rule[0]);
-  }
-
-  subset ? day5b(lines) : day5a(lines);
+  print(lines
+      .sublist(rulesEnd + 1)
+      .map((e) => e.split(",").map((s) => int.parse(s)).toList())
+      .fold<int>(
+          0,
+          (p, e) =>
+              p +
+              (e.isSorted(comp) != isPart2
+                  ? e.sorted(comp)[e.length >> 1]
+                  : 0)));
 }
-
-int comp(int a, int b) {
-  if (a == b) return 0;
-  if (ordering[a]![0].contains(b)) return -1;
-  return 1;
-}
-
-void day5a(List<String> lines) {
-  int count = 0;
-  for (nline++; nline < lines.length; nline++) {
-    List<int> pages = lines[nline].split(",").map((e) => int.parse(e)).toList();
-    if (pages.isSorted(comp)) {
-      count += pages[pages.length >> 1];
-    }
-  }
-  print(count);
-}
-
-void day5b(List<String> lines) {
-  int count = 0;
-  for (nline++; nline < lines.length; nline++) {
-    List<int> pages = lines[nline].split(",").map((e) => int.parse(e)).toList();
-    if (!pages.isSorted(comp)) {
-      pages.sort(comp);
-      count += pages[pages.length >> 1];
-    }
-  }
-  print(count);
-}
-
-
