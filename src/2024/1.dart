dd5910f3650f0dc34a9e68c1ae3471fedc5c506e
Fluxward <howard@fluxkeyboard.com>
2024-12-08 18:42:47 +1100

    compress 1, 2

--- src/2024/1.dart
+++ src/2024/1.dart
@@ -1,25 +1,18 @@
-import 'dart:io';
-
 import 'package:collection/collection.dart';
 
 import '../common.dart';
 
+Map<int, int> freq = {};
 void d1(bool s) {
-  s ? day1s() : day1();
-}
-
-void day1() {
-  print(IterableZip(
-          IterableZip(getLines().map((l) => l.split('   ').map(int.parse)))
-              .map((l) => l.sorted((a, b) => a - b)))
-      .fold<int>(0, (p, e) => p + (e.first - e.last).abs()));
-}
-
-void day1s() {
-  List<List<int>> input =
-      IterableZip(getLines().map((l) => l.split('   ').map(int.parse)).toList())
-          .toList();
-  Map<int, int> freq = {};
-  input[1].forEach((e) => freq[e] = (freq[e] ?? 0) + 1);
-  print(input[0].fold<int>(0, (p, e) => p + ((freq[e]??0) * e)));
+  var input = IterableZip(getLines().map((l) => l.split('   ').map(int.parse)));
+  if (s)
+    print(input.first.fold<int>(
+        0,
+        (p, e) =>
+            p +
+            freq.putIfAbsent(
+                e, () => e * input.last.where((e2) => e2 == e).length)));
+  else
+    print(IterableZip(input.map((l) => l.sorted((a, b) => a - b)))
+        .fold<int>(0, (p, e) => p + (e.first - e.last).abs()));
 }
--- src/2024/2.dart
+++ src/2024/2.dart
@@ -1,46 +1,25 @@
-import 'dart:io';
-
 import 'package:collection/collection.dart';
 
-void d2(bool s) => s ? day2s() : day2();
+import '../common.dart';
 
-bool isSafe(List<int> l) {
-  bool safe = false;
-  safe = safe || l.foldIndexed<bool>(true, (i, p, e) => p && (i < 1 || (0 < (e - l[i-1]).abs() && 4 > (e - l[i-1]).abs())));
+void d2(bool s) => print(getLines()
+    .map((l) => [...l.split(" ").map(int.parse)])
+    .where(s ? isSafeWithSkip : isSafe)
+    .length);
 
-  safe = safe && (l.foldIndexed<bool>(true, (i, p, e) => p && (i < 1 || l[i-1] < e)) || l.foldIndexed<bool>(true, (i, p, e) => p && (i < 1 || l[i-1] > e)));
+int comp(int a, int b) => a - b;
 
-  return safe;
-}
+bool isConsistent(List<int> l) => l.isSorted(comp) || l.reversed.isSorted(comp);
 
-bool isSafeWithSkip(List<int> l) {
-  if (isSafe(l)) return true;
+bool isSafe(List<int> l) =>
+    isConsistent(l) &&
+    (l.length < 2 ||
+        l
+            .sublist(1)
+            .mapIndexed((i, e) => (l[i] - e).abs())
+            .every((d) => d > 0 && d < 4));
 
-  for (int i = 0; i < l.length; i++) {
-    if (isSafe([...l.whereNotIndexed((ind, e) => i == ind)])) return true;
-  }
-
-  return false;
-}
-
-void day2() {
-  int count = 0;
-  for (String? line = stdin.readLineSync();
-      line != null;
-      line = stdin.readLineSync()) {
-    List<int> report = [...line.split(" ").map((e) => int.parse(e))];
-    count = isSafe(report) ? count + 1 : count;
-  }
-  print(count);
-}
-
-void day2s() {
-  int count = 0;
-  for (String? line = stdin.readLineSync();
-      line != null;
-      line = stdin.readLineSync()) {
-    List<int> report = [...line.split(" ").map((e) => int.parse(e))];
-    count = isSafeWithSkip(report) ? count + 1 : count;
-  }
-  print(count);
-}
+bool isSafeWithSkip(List<int> l) =>
+    isSafe(l) ||
+    l.foldIndexed<bool>(false,
+        (i, p, e) => p || isSafe([...l.whereNotIndexed((i0, e0) => i0 == i)]));
