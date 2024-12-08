3c310e14cffa077e514709a28981c69aa70be288
Fluxward <howard@fluxkeyboard.com>
2024-12-08 18:52:05 +1100

    compress 3

--- src/2024/3.dart
+++ src/2024/3.dart
@@ -2,30 +2,15 @@ import 'dart:io';
 
 import '../common.dart';
 
-void d3(bool s) => s ? day3s() : day3();
-void day3s() {
-  print(RegExp(r"(?:don\'t\(\)(?:.(?<!do\(\)))*do\(\))|(?:mul\((\d*),(\d*)\))")
-      .allMatches([
-        for (String? line = stdin.readLineSync();
-            line != null;
-            line = stdin.readLineSync())
-          line
-      ].join())
-      .fold<int>(
-          0,
-          (p, e) =>
-              p +
-              (e.group(0)![0] != 'd'
-                  ? e.groups([1, 2]).fold<int>(1, (p, f) => p * int.parse(f!))
-                  : 0)));
-}
+var res =
+    RegExp(r"(?:don\'t\(\)(?:.(?<!do\(\)))*do\(\))|(?:mul\((\d*),(\d*)\))");
+var re = RegExp(r"mul\((\d*),(\d*)\)");
 
-void day3() {
-  print(RegExp(r"mul\((\d*),(\d*)\)").allMatches([
-    for (String? line = stdin.readLineSync();
-        line != null;
-        line = stdin.readLineSync())
-      line
-  ].join()).fold<int>(
-      0, (p, e) => p + e.groups([1, 2]).fold(1, (p, f) => p * int.parse(f!))));
-}
+int ma(RegExpMatch m) => m.groups([1, 2]).fold(1, (p, f) => p * int.parse(f!));
+int mb(RegExpMatch m) => m.group(0)![0] != 'd'
+    ? m.groups([1, 2]).fold<int>(1, (p, f) => p * int.parse(f!))
+    : 0;
+
+void d3(bool s) => print((s ? res : re)
+    .allMatches(getLines().join())
+    .fold<int>(0, (p, e) => p + (s ? mb(e) : mb(e))));
