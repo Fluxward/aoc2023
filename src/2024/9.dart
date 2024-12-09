7066a84dc16fc01cf8330daf2d0b71f6bf72b9be
Fluxward <howard@fluxkeyboard.com>
2024-12-10 05:00:08 +1100

    cc

--- src/2024/9.dart
+++ src/2024/9.dart
@@ -5,30 +5,29 @@ import 'package:collection/collection.dart';
 
 d9(bool s) {
   List<int> input = stdin.readLineSync()!.split("").map(int.parse).toList();
-  var data = input.whereIndexed((i, e) => (i % 2) == 0).toList();
-  var space = input.whereIndexed((i, e) => (i % 2) == 1).toList();
 
-  int totalSpace = data.reduce((v, w) => v + w);
+  int totalSpace =
+      input.whereIndexed((i, e) => i % 2 == 0).reduce((v, w) => v + w);
 
   int count = 0;
   int i = 0;
   int id = 0;
-  int lastId = data.length - 1;
+  int lastId = (input.length - 1) ~/ 2;
   while (i < totalSpace && id < lastId) {
-    count += (id * (2 * i + data[id] - 1) * data[id]) ~/ 2;
-    i += data[id];
-    data[id] = 0;
+    count += (id * (2 * i + input[2 * id] - 1) * input[2 * id]) ~/ 2;
+    i += input[2 * id];
+    input[2 * id] = 0;
 
-    while (space[id] > 0) {
-      if (data[lastId] == 0 && lastId-- == id) {
+    while (input[2 * id + 1] > 0) {
+      if (input[2 * lastId] == 0 && lastId-- == id) {
         print(count);
         return;
       }
-      int dif = min(space[id], data[lastId]);
+      int dif = min(input[2 * id + 1], input[2 * lastId]);
       count += (lastId * (2 * i + dif - 1) * dif) ~/ 2;
 
-      space[id] -= dif;
-      data[lastId] -= dif;
+      input[2 * id + 1] -= dif;
+      input[2 * lastId] -= dif;
       i += dif;
     }
     id++;
