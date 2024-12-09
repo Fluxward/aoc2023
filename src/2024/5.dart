bf7259fc1874a9c565101f4549ba9d36b5b6f38b
Fluxward <howard@fluxkeyboard.com>
2024-12-10 00:06:16 +1100

    compress 5

--- src/2024/5.dart
+++ src/2024/5.dart
@@ -3,24 +3,19 @@ import 'package:collection/collection.dart';
 
 void d5(bool isPart2) {
   Map<int, Set<int>> ordering = {};
-  int comp(int a, int b) => ordering[a]?.contains(b) ?? false ? -1 : 1;
+  int cm(int a, int b) => ordering[a]?.contains(b) ?? false ? -1 : 1;
 
   List<String> lines = getLines();
   int rulesEnd = lines.indexWhere((line) => line.isEmpty);
 
   lines
-      .sublist(0, rulesEnd)
+      .take(rulesEnd)
       .map((line) => line.split('|').map((e) => int.parse(e)).toList())
       .forEach((rule) => ordering.putIfAbsent(rule[0], Set.new).add(rule[1]));
 
   print(lines
-      .sublist(rulesEnd + 1)
+      .skip(rulesEnd + 1)
       .map((line) => line.split(",").map((s) => int.parse(s)).toList())
-      .fold<int>(
-          0,
-          (p, e) =>
-              p +
-              (e.isSorted(comp) != isPart2
-                  ? e.sorted(comp)[e.length >> 1]
-                  : 0)));
+      .map((e) => e.isSorted(cm) != isPart2 ? e.sorted(cm)[e.length >> 1] : 0)
+      .reduce((p, e) => p + e));
 }
