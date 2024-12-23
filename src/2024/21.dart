0528551effc6d136b8bb7664a06b492a50f8bdfb
Fluxward <howard@fluxkeyboard.com>
2024-12-24 03:36:22 +1100

    solve 21a

--- src/2024/21.dart
+++ src/2024/21.dart
@@ -51,13 +51,19 @@ enum btn {
 
   @override
   String toString() {
-    switch(this) {
-      case u: return "^";
-      case l: return '<';
-      case r: return '>';
-      case d: return 'v';
-      case a: return 'A';
-      case na: return 'X';
+    switch (this) {
+      case u:
+        return "^";
+      case l:
+        return '<';
+      case r:
+        return '>';
+      case d:
+        return 'v';
+      case a:
+        return 'A';
+      case na:
+        return 'X';
     }
   }
 }
@@ -67,9 +73,14 @@ List<List<btn>> dp = [
   [btn.l, btn.d, btn.r]
 ];
 
-Map<btn, P> dpm = Map.fromEntries(dp
-    .mapIndexed((r, l) => l.mapIndexed((c, i) => MapEntry(i, P(r, c))))
-    .flattened);
+Map<btn, P> dpm = {
+  btn.na: P(0, 0),
+  btn.u: P(0, 1),
+  btn.a: P(0, 2),
+  btn.l: P(1, 0),
+  btn.d: P(1, 1),
+  btn.r: P(1, 2),
+};
 
 Set<P> vdp = Set.from(dpm.values.whereNot((v) => v == P(0, 0)));
 
@@ -84,22 +95,27 @@ final P dpA = P(0, 2);
 final P kpA = P(3, 2);
 
 int hr0(P a, P b) => a.mhd(b);
-Map<(List<P>, List<P>, int), int> __rab = HashMap(
+Map<(List<P>, List<P>, int), (int, List<btn>)> __rab = HashMap(
     equals: (p0, p1) =>
         ListEquality<P>().equals(p0.$1, p1.$1) &&
         ListEquality<P>().equals(p0.$2, p1.$2) &&
         p0.$3 == p1.$3,
-    hashCode: (p0) => Object.hashAll([Object.hashAll([p0.$1, p0.$2]), p0.$3]));
-int rab(List<P> a, List<P> b, l) =>
+    hashCode: (p0) => Object.hashAll([
+          Object.hashAll([p0.$1, p0.$2]),
+          p0.$3
+        ]));
+(int, List<btn>) rab(List<P> a, List<P> b, l) =>
     __rab.putIfAbsent((a, b, l), () => _rab(a, b, l));
 
 class St21 implements Comparable<St21> {
   final List<P> r;
 
+  final List<btn> p;
+
   final int d;
   final St21? pr;
 
-  St21(this.r, this.d, this.pr);
+  St21(this.r, this.d, this.pr, this.p);
 
   @override
   int compareTo(St21 other) {
@@ -109,9 +125,41 @@ class St21 implements Comparable<St21> {
 
 P dbtn(dir d) => dpm[dbm[d]]!;
 
-int _rab(List<P> a, List<P> b, int l) {
-  if (l == 1) return hr0(a.first, b.first);
-  PriorityQueue<St21> q = PriorityQueue()..add(St21(a, 0, null));
+Map<(P, P), List<btn>> argh = {
+  (P(0, 1), P(0, 1)): [],
+  (P(0, 1), P(0, 2)): [btn.r],
+  (P(0, 1), P(1, 0)): [btn.l, btn.d],
+  (P(0, 1), P(1, 1)): [btn.d],
+  (P(0, 1), P(1, 2)): [btn.d, btn.r],
+  (P(1, 1), P(0, 1)): [btn.u],
+  (P(1, 1), P(0, 2)): [btn.r, btn.u],
+  (P(1, 1), P(1, 0)): [btn.l],
+  (P(1, 1), P(1, 1)): [],
+  (P(1, 1), P(1, 2)): [btn.r],
+  (P(0, 2), P(0, 1)): [btn.l],
+  (P(0, 2), P(0, 2)): [],
+  (P(0, 2), P(1, 0)): [btn.d, btn.l, btn.l],
+  (P(0, 2), P(1, 1)): [btn.d, btn.l],
+  (P(0, 2), P(1, 2)): [btn.d],
+  (P(1, 0), P(0, 1)): [btn.r, btn.u],
+  (P(1, 0), P(0, 2)): [btn.r, btn.r, btn.u],
+  (P(1, 0), P(1, 0)): [],
+  (P(1, 0), P(1, 1)): [btn.r],
+  (P(1, 0), P(1, 2)): [btn.r, btn.r],
+  (P(1, 2), P(0, 1)): [btn.l, btn.u],
+  (P(1, 2), P(0, 2)): [btn.u],
+  (P(1, 2), P(1, 0)): [btn.l, btn.l],
+  (P(1, 2), P(1, 1)): [btn.l],
+  (P(1, 2), P(1, 2)): [],
+};
+
+(int, List<btn>) _rab(List<P> a, List<P> b, int l) {
+  if (l == 1) {
+    int ret = hr0(a.first, b.first);
+    //print("$a -> $b: $ret moves");
+    return (ret, argh[(a.first, b.first)]!);
+  }
+  PriorityQueue<St21> q = PriorityQueue()..add(St21(a, 0, null, []));
   Set<List<P>> seen = HashSet(
       equals: (la, lb) => ListEquality<P>().equals(la, lb),
       hashCode: Object.hashAll);
@@ -122,42 +170,50 @@ int _rab(List<P> a, List<P> b, int l) {
     seen.add(c.r);
 
     if (c.r.last == b.last) {
-      q.add(St21(
-          b,
-          c.d +
-               rab(c.r.sublist(0, l - 1), b.sublist(0, l - 1), l - 1),
-          c));
+      (int, List<btn>) pt =
+          rab(c.r.sublist(0, l - 1), b.sublist(0, l - 1), l - 1);
+      q.add(St21(b, c.d + pt.$1, c, [...c.p, ...pt.$2]));
       continue;
     }
 
     for (dir d in dir.values) {
       P n = d + c.r.last;
       P dp = dbtn(d);
-      if ((l == 3 && !vkp.contains(n)) || (l < 3 && !vdp.contains(n))) continue;
+      if ((l == tl && !vkp.contains(n)) || (l < tl && !vdp.contains(n)))
+        continue;
       int cost = c.d;
       List<P> np = [...List.filled(l - 2, dpA), dp, n];
-      cost += rab(c.r.sublist(0, l - 1), np.sublist(0, l - 1), l - 1);
+      (int, List<btn>) r =
+          rab(c.r.sublist(0, l - 1), np.sublist(0, l - 1), l - 1);
+      cost += r.$1;
       // press A
       cost++;
 
       //print(cost);
-      q.add(St21(np, cost, c));
+      q.add(St21(np, cost, c, [...c.p, ...r.$2, btn.a]));
     }
   }
-  print("$a -> $b: ${q.first.d} moves");
-  return q.first.d;
+  //print("$a -> $b: ${q.first.d} moves");
+  return (q.first.d, q.first.p);
 }
 
-int complexity(String s) =>
-    //int.parse(s.substring(0, s.length - 1)) *
-    s.split("").map((c) => int.parse(c, radix: 16)).foldIndexed(
-        0,
-        (i, p, e) =>
-            p + 1 +
-            (rab([dpA, dpA, i == 0 ? kpA : kpm[i - 1]!], [dpA, dpA, kpm[e]!],
-                3)));
+int tl = 3;
+(int, List<btn>) complexity(List<int> li) =>
+    li.foldIndexed<(int, List<btn>)>((0, []), (i, p, e) {
+      //print("Moving from ${i == 0 ? 'A' : li[i - 1]} to $li");
+      (int, List<btn>) r = (rab(
+          [...List.filled(tl - 1, dpA), i == 0 ? kpA : kpm[li[i - 1]]!],
+          [...List.filled(tl - 1, dpA), kpm[e]!],
+          tl));
+      return ((p.$1 + 1 + r.$1), [...p.$2, ...r.$2, btn.a]);
+    });
 void d21(bool sub) {
+  List<List<btn>> bp = [];
   List<String> input = getLines();
-  //print(input.map(complexity).reduce((a, b) => a + b));
-  input.map(complexity).forEach(print);
+  List<List<int>> ii = input
+      .map((i) => i.split("").map((c) => int.parse(c, radix: 16)).toList())
+      .toList();
+  print(ii.map(complexity).foldIndexed<int>(0, (i, p, e) {
+    return p + e.$1 * int.parse(input[i].substring(0, input[i].length - 1));
+  }));
 }
