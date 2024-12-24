540c8840fe7ce2ba239f433e2278c1ee399502d8
Fluxward <howard@fluxkeyboard.com>
2024-12-24 15:05:54 +1100

    tail refactor

--- src/2024/21.dart
+++ src/2024/21.dart
@@ -1,5 +1,6 @@
 import 'dart:collection';
 import 'dart:core';
+import 'dart:math';
 
 import 'package:collection/collection.dart';
 
@@ -95,7 +96,7 @@ final P dpA = P(0, 2);
 final P kpA = P(3, 2);
 
 int hr0(P a, P b) => a.mhd(b);
-Map<(List<P>, List<P>, int), (int, List<btn>)> __rab = HashMap(
+Map<(List<P>, List<P>, int), int> __rab = HashMap(
     equals: (p0, p1) =>
         ListEquality<P>().equals(p0.$1, p1.$1) &&
         ListEquality<P>().equals(p0.$2, p1.$2) &&
@@ -104,18 +105,16 @@ Map<(List<P>, List<P>, int), (int, List<btn>)> __rab = HashMap(
           Object.hashAll([p0.$1, p0.$2]),
           p0.$3
         ]));
-(int, List<btn>) rab(List<P> a, List<P> b, l) =>
+int rab(List<P> a, List<P> b, l) =>
     __rab.putIfAbsent((a, b, l), () => _rab(a, b, l));
 
 class St21 implements Comparable<St21> {
   final List<P> r;
 
-  final List<btn> p;
-
   final int d;
   final St21? pr;
 
-  St21(this.r, this.d, this.pr, this.p);
+  St21(this.r, this.d, this.pr);
 
   @override
   int compareTo(St21 other) {
@@ -153,13 +152,13 @@ Map<(P, P), List<btn>> argh = {
   (P(1, 2), P(1, 2)): [],
 };
 
-(int, List<btn>) _rab(List<P> a, List<P> b, int l) {
+int _rab(List<P> a, List<P> b, int l) {
   if (l == 1) {
     int ret = hr0(a.first, b.first);
     //print("$a -> $b: $ret moves");
-    return (ret, argh[(a.first, b.first)]!);
+    return ret;
   }
-  PriorityQueue<St21> q = PriorityQueue()..add(St21(a, 0, null, []));
+  PriorityQueue<St21> q = PriorityQueue()..add(St21(a, 0, null));
   Set<List<P>> seen = HashSet(
       equals: (la, lb) => ListEquality<P>().equals(la, lb),
       hashCode: Object.hashAll);
@@ -170,9 +169,15 @@ Map<(P, P), List<btn>> argh = {
     seen.add(c.r);
 
     if (c.r.last == b.last) {
-      (int, List<btn>) pt =
-          rab(c.r.sublist(0, l - 1), b.sublist(0, l - 1), l - 1);
-      q.add(St21(b, c.d + pt.$1, c, [...c.p, ...pt.$2]));
+      int pt = 0; //rab(c.r, b, l - 1);
+      if (l > 3) {
+        pt = rab([dpA, ...c.r.take(2)], [dpA, ...b.take(2)], l - 1);
+      } else if (l == 3) {
+        pt = rab([...c.r.take(2)], [...b.take(2)], l - 1);
+      } else {
+        pt = rab([c.r.first], [b.first], 1);
+      }
+      q.add(St21(b, c.d + pt, c));
       continue;
     }
 
@@ -182,38 +187,41 @@ Map<(P, P), List<btn>> argh = {
       if ((l == tl && !vkp.contains(n)) || (l < tl && !vdp.contains(n)))
         continue;
       int cost = c.d;
-      List<P> np = [...List.filled(l - 2, dpA), dp, n];
-      (int, List<btn>) r =
-          rab(c.r.sublist(0, l - 1), np.sublist(0, l - 1), l - 1);
-      cost += r.$1;
+      List<P> np;
+      if (l >= 3) {
+        np = [dpA, dp, n];
+      } else {
+        np = [dp, n];
+      }
+      cost += rab(
+          c.r.sublist(0, np.length - 1), np.sublist(0, np.length - 1), l - 1);
       // press A
       cost++;
 
       //print(cost);
-      q.add(St21(np, cost, c, [...c.p, ...r.$2, btn.a]));
+      q.add(St21(np, cost, c));
     }
   }
   //print("$a -> $b: ${q.first.d} moves");
-  return (q.first.d, q.first.p);
+  return q.first.d;
 }
 
-int tl = 3;
-(int, List<btn>) complexity(List<int> li) =>
-    li.foldIndexed<(int, List<btn>)>((0, []), (i, p, e) {
+int tl = 6;
+int complexity(List<int> li) => li.foldIndexed<int>(0, (i, p, e) {
       //print("Moving from ${i == 0 ? 'A' : li[i - 1]} to $li");
-      (int, List<btn>) r = (rab(
-          [...List.filled(tl - 1, dpA), i == 0 ? kpA : kpm[li[i - 1]]!],
-          [...List.filled(tl - 1, dpA), kpm[e]!],
-          tl));
-      return ((p.$1 + 1 + r.$1), [...p.$2, ...r.$2, btn.a]);
+      int r = (rab(
+          [dpA, dpA, i == 0 ? kpA : kpm[li[i - 1]]!], [dpA, dpA, kpm[e]!], tl));
+      return p + 1 + r;
     });
 void d21(bool sub) {
+  Stopwatch st = Stopwatch()..start();
   List<List<btn>> bp = [];
   List<String> input = getLines();
   List<List<int>> ii = input
       .map((i) => i.split("").map((c) => int.parse(c, radix: 16)).toList())
       .toList();
   print(ii.map(complexity).foldIndexed<int>(0, (i, p, e) {
-    return p + e.$1 * int.parse(input[i].substring(0, input[i].length - 1));
+    return p + e * int.parse(input[i].substring(0, input[i].length - 1));
   }));
+  print(st.elapsed);
 }
